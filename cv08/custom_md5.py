8166750ee7bb9bac11e0462479908cba41c9545d
Leonid <lemalaxz2@gmail.com>
2025-04-01 19:33:05 +0200

    ...

--- cv08/custom_md5.py
+++ cv08/custom_md5.py
@@ -1,6 +1,3 @@
-import struct
-
-
 def left_rotate(x, amount):
     return ((x << amount) | (x >> (8 - amount))) & 0xFF
 
@@ -29,28 +26,26 @@ def custom_md5(message):
     rotate_vals = [3, 1, 5, 2]
     funcs = [F2, F1, F4, F3]
 
-    # Преобразование в байты
     message_bytes = message.encode('utf-8')
 
     count = 0
 
-    # Запуск хэширования
     for byte in message_bytes:
-        if count > 0 and count % 4 == 0:
+        if count == 4:
             count = 0
         func = funcs[count]
-        K_i = K[count] & 0xFF
+        K_i = K[count]
         rot = rotate_vals[count]
         temp = (A + func(B, C, D) + byte + K_i) & 0xFF
         temp = left_rotate(temp, rot) & 0xFF
-        A, B, C, D = D, temp, B, C  # Правильное обновление
+        temp = (temp + B) & 0xFF
+        A, B, C, D = D, temp, B, C
         count += 1
 
 
     return f"{A:02x}{B:02x}{C:02x}{D:02x}"
 
-
-# Пример использования
-P = "Kdo implementuje tuto hash funkci, dostane 2 bodiky."
-hash_value = custom_md5(P)
-print("Hash:", hash_value)
+if __name__ == '__main__':
+    P = "Kdo implementuje tuto hash funkci, dostane 2 bodiky."
+    hash_value = custom_md5(P)
+    print("Hash:", hash_value)
